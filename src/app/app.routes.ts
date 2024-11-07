9e4baa20c8e4fd2756ac4d8f877cf7d7a98123fc
Eyeyuel <eyueltk@gmail.com>
2024-11-07 08:30:17 +0300

    Layouts updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,18 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './features/components/layout/layout.component';
 
 export const routes: Routes = [
-   {
-    path:"",
-    component:LayoutComponent
-   },
-   {
+  {
+    path: 'shared',
+    loadChildren: () =>
+      import('./shared/shared.module').then((m) => m.SharedModule),
+  },
+  {
     path: 'admin',
-    loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule)
-   },
-  
+    loadChildren: () =>
+      import('./features/admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+  },
 ];
--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -1,26 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedLayoutComponent } from './components/shared-layout/shared-layout.component';
+import { PropertiesComponent } from './components/properties/properties.component';
+import { AgentsComponent } from './components/agents/agents.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   {
-    path:"shared",
-    component:SharedLayoutComponent,
-    children:[
+    path: '',
+    component: SharedLayoutComponent,
+    children: [
       {
-        path:"",
-        component:SharedLayoutComponent
+        path: 'properties',
+        component: PropertiesComponent,
       },
       {
-        path:"navbar",
-        
-      }
-    ]
-  }
+        path: 'agents',
+        component: AgentsComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: '',
+        component: SharedLayoutComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SharedRoutingModule { }
+export class SharedRoutingModule {}
